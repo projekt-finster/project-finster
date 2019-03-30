3f1b34594c3a1450252b9ee2120c627044a97f8e
Blahkhan <mcmatie@wp.pl>
2019-03-30 09:41:05 +0100

    początki konsoli

---
+++ Scripts/Inne/Consola/MainConsol.cs
@@ -0,0 +1,23 @@
+﻿//By Blahkhan
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainConsol : MonoBehaviour
+{
+
+    private void Update()
+    {
+        
+    }
+
+    void ShowCloaseConsole()
+    {
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+
+        }
+    }
+
+}
